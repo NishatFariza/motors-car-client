feef296cfef1f9680122dc18eeef1aa50d281a21
NishatFariza <nishatfarzia78@gmail.com>
2022-04-29 23:27:31 +0600

    react route add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+import { Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
