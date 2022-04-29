793288d2aff3d7fae6620562db7c5689479e3463
NishatFariza <nishatfarzia78@gmail.com>
2022-04-29 23:45:37 +0600

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import { Routes } from 'react-router-dom';
 import './App.css';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
-        
+
       </Routes>
     </div>
   );
