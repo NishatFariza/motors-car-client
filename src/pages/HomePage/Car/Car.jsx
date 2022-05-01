47b7814ce22aae48f6cae6755cbc5fa7c9b0401b
NishatFariza <nishatfarzia78@gmail.com>
2022-05-02 02:12:17 +0600

    create car component

---
+++ src/pages/HomePage/Car/Car.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Car = () => {
+  return (
+    <div>
+      <h1>this is car</h1>
+    </div>
+  );
+};
+
+export default Car;
--- src/pages/HomePage/InventoryItems/InventoryItems.jsx
+++ src/pages/HomePage/InventoryItems/InventoryItems.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Car from "../Car/car";
 
 const InventoryItems = () => {
   const [cars, setCars] = useState();
@@ -8,6 +9,7 @@ const InventoryItems = () => {
     const url = `http://localhost:5000/cars`;
     (async () => {
       const { data } = await axios.get(url);
+      setCars(data);
       console.log(data);
     })();
   }, []);
@@ -15,7 +17,11 @@ const InventoryItems = () => {
   return (
     <div className="container mx-auto py-12">
       <h2 className="text-center text-2xl font-semibold">Recent Cars</h2>
-      <div className=""></div>
+      <div>
+        {cars.map((car) => (
+          <Car car={car}></Car>
+        ))}
+      </div>
     </div>
   );
 };
