e8aaf5c226592ac20ae9ff049fc80a9e7f618abb
NishatFariza <nishatfarzia78@gmail.com>
2022-05-01 15:36:26 +0600

    set envirenment variable for firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCha_RRU78uuq1ZYxjfEm2OW_X3XzqTPvI",
-  authDomain: "motors-car-11.firebaseapp.com",
-  projectId: "motors-car-11",
-  storageBucket: "motors-car-11.appspot.com",
-  messagingSenderId: "172170005393",
-  appId: "1:172170005393:web:1f18b8b71ecedb141b6830"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
