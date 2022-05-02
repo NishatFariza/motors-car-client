45b88e42d6ea4463a75fed00a66aecb1fc392841
NishatFariza <nishatfarzia78@gmail.com>
2022-05-03 01:02:15 +0600

    create not found component

---
+++ src/pages/Shared/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>THE PAGE YOU ARE LOOKING FOR DOES NOT EXIST</h2>
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
