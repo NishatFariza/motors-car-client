58685ed656be771b835f6875017771d7a6de1ba7
NishatFariza <nishatfarzia78@gmail.com>
2022-05-02 20:50:23 +0600

    testimonial icon add

--- src/pages/HomePage/Testimonial/Testimonial.jsx
+++ src/pages/HomePage/Testimonial/Testimonial.jsx
@@ -7,44 +7,44 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from "swiper";
 
 const Testimonial = () => {
   return (
     <div className="bg-[url('https://i.ibb.co/y658DKR/testimonial-bg.jpg')] bg-cover h-[90vh]">
       <Swiper
+        slidesPerView={1}
         spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        loop={true}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
+        modules={[Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-          <div className="flex justify-center items-center ">
-            <div>
-              <img src={testimonial1} alt="" />
+          <div className="flex justify-center items-center h-[80vh] w-[40%] mx-auto">
+            <div className="text-center ">
+              <img className="mx-auto" src={testimonial1} alt="" />
               <h4>JOANA OLSEN</h4>
               <p>Customer</p>
-              <blockquote>
+              <blockquote className="text-white">
                 Impressed! I have installed 50+ themes. This is by far the best
                 one I have ever worked on. Easy to use and loaded with
                 functions. Support is also great! A++”
               </blockquote>
-              <FaQuoteRight className="text-yellow-500 text-3xl"></FaQuoteRight>
+
+              <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex justify-center items-center ">
-            <div>
-              <img src={testimonial1} alt="" />
+          <div className="flex justify-center items-center h-[80vh] w-[40%] mx-auto">
+            <div className="text-center">
+              <div>
+                <img className="mx-auto" src={testimonial1} alt="" />
+              </div>
               <h4>JOANA OLSEN</h4>
               <p>Customer</p>
               <blockquote>
@@ -52,14 +52,14 @@ const Testimonial = () => {
                 one I have ever worked on. Easy to use and loaded with
                 functions. Support is also great! A++”
               </blockquote>
-              <FaQuoteRight className="text-yellow-500 text-3xl"></FaQuoteRight>
+              <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
             </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex justify-center items-center ">
-            <div>
-              <img src={testimonial1} alt="" />
+          <div className="flex justify-center items-center h-[80vh] w-[40%] mx-auto">
+            <div className="text-center">
+              <img className="mx-auto" src={testimonial1} alt="" />
               <h4>JOANA OLSEN</h4>
               <p>Customer</p>
               <blockquote>
@@ -67,7 +67,7 @@ const Testimonial = () => {
                 one I have ever worked on. Easy to use and loaded with
                 functions. Support is also great! A++”
               </blockquote>
-              <FaQuoteRight className="text-yellow-500 text-3xl"></FaQuoteRight>
+              <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
             </div>
           </div>
         </SwiperSlide>
