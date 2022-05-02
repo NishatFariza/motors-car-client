ab2963cedb86efe5074d1b8c8a57f32172aea70d
NishatFariza <nishatfarzia78@gmail.com>
2022-05-02 22:15:49 +0600

    testimonial text add

--- src/pages/HomePage/Testimonial/Testimonial.jsx
+++ src/pages/HomePage/Testimonial/Testimonial.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaQuoteRight } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import testimonial1 from "../../../images/testimonial1.jpg";
+import "./Testimonial.css";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -25,14 +26,19 @@ const Testimonial = () => {
       >
         <SwiperSlide>
           <div className="flex justify-center items-center h-[80vh] w-[40%] mx-auto">
-            <div className="text-center ">
-              <img className="mx-auto" src={testimonial1} alt="" />
-              <h4>JOANA OLSEN</h4>
-              <p>Customer</p>
-              <blockquote className="text-white">
-                Impressed! I have installed 50+ themes. This is by far the best
-                one I have ever worked on. Easy to use and loaded with
-                functions. Support is also great! A++”
+            <div className="text-center py-8">
+              <img
+                className="mx-auto border-8 testimonial-img"
+                src={testimonial1}
+                alt=""
+              />
+              <h4 className="mt-6 text-xl text-white">Khalid</h4>
+              <p className="mb-6 text-stone-400">Customer</p>
+              <blockquote className="text-white text-xl">
+                “Impressed! We really only knock the R8 Performance for the
+                sport seats that have no seatback adjustments. The side
+                bolstering is also a bit too aggressive, which could be
+                problematic for wider-bodied passengers.! A++”
               </blockquote>
 
               <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
@@ -45,12 +51,14 @@ const Testimonial = () => {
               <div>
                 <img className="mx-auto" src={testimonial1} alt="" />
               </div>
-              <h4>JOANA OLSEN</h4>
+              <h4>Donald Simpson</h4>
               <p>Customer</p>
-              <blockquote>
-                Impressed! I have installed 50+ themes. This is by far the best
-                one I have ever worked on. Easy to use and loaded with
-                functions. Support is also great! A++”
+              <blockquote className="text-white text-xl">
+                “With its recent 2021 refresh, we don't expect too many changes
+                for 2023, and Tesla hasn't confirmed anything. Tesla will likely
+                add new features via over-the-air updates, and to stay
+                competitive, there is a good chance the driving range will
+                slightly increase.”
               </blockquote>
               <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
             </div>
@@ -62,10 +70,11 @@ const Testimonial = () => {
               <img className="mx-auto" src={testimonial1} alt="" />
               <h4>JOANA OLSEN</h4>
               <p>Customer</p>
-              <blockquote>
-                Impressed! I have installed 50+ themes. This is by far the best
-                one I have ever worked on. Easy to use and loaded with
-                functions. Support is also great! A++”
+              <blockquote className="text-white text-xl">
+                “The Highlander has been hauling large families around for over
+                20 years. It serves as Toyota's midsize three-row SUV that's
+                positioned above the smaller two-row RAV4 and below the larger
+                truck-like Sequoia.”
               </blockquote>
               <FaQuoteRight className="text-yellow-500 text-4xl mx-auto"></FaQuoteRight>
             </div>
