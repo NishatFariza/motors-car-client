fd1fdfd908cb623be6f14d13bda73732adc6a37e
NishatFariza <nishatfarzia78@gmail.com>
2022-05-03 05:49:05 +0600

    faq text added

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -15,61 +15,133 @@ const Blogs = () => {
         FREQUENTLY ASK <span className="text-yellow-400">QUESTION</span>
       </h2>
       <div className="h-1 w-24 rounded-lg bg-blue-400 mx-auto mb-12"></div>
-      <Accordion className="border rounded box-shadow duration-500">
-        <AccordionItem>
-          <AccordionItemHeading className="border-b  p-3 rounded">
+      <Accordion className="rounded duration-500 ">
+        <AccordionItem className="box-shadow rounded duration-500">
+          <AccordionItemHeading className="border-t  py-6 px-3 rounded">
             <AccordionItemButton className="text-2xl font-bold text-blue-400 flex items-center">
               <AiFillQuestionCircle className="mr-2"></AiFillQuestionCircle>
-              WHAT IS THE DIFFERENCE BETWEEN JAVASCRIPT AND NODEJS?
+              WHAT IS THE DIFFERENCE BETWEEN JAVASCRIPT AND NODE.JS?
             </AccordionItemButton>
           </AccordionItemHeading>
-          <AccordionItemPanel>
-            <p>
-              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
-              occaecat ut occaecat consequat est minim minim esse tempor laborum
-              consequat esse adipisicing eu reprehenderit enim.
+          <AccordionItemPanel className="border-b">
+            <p className="px-4 pt-8">
+              <strong className="underline primary-color font-bold text-justify">
+                JavaScript:
+              </strong>
+              Javascript is a programming language that is used for writing
+              scripts on the website. It is basically used on the client-side.
+              It is capable enough to add HTML and play with the DOM. Javascript
+              can run in any browser engine as like JS core in safari and
+              Spidermonkey in Firefox. It is the upgraded version of ECMA scrip
+              that uses Chrome’s V8 engine written in C++. Some of the
+              javascript frameworks are RamdaJS, TypedJS, etc.
+            </p>
+            <p className="px-4 py-8">
+              <strong className="underline primary-color font-bold">
+                NodeJS:
+              </strong>
+              NodeJS is a Javascript runtime environment. It is mostly used on
+              the server-side. Nodejs does not have capability to add HTML tags.
+              V8 is the Javascript engine inside of node.js that parses and runs
+              Javascript. Nodejs is written in C, C++ and Javascript. Some of
+              the Nodejs modules are Lodash, express etc. These modules are to
+              be imported from npm.
             </p>
           </AccordionItemPanel>
         </AccordionItem>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton>
-              Is free will real or just an illusion?
+        <AccordionItem className="box-shadow border-y rounded duration-500">
+          <AccordionItemHeading className="border-y-b  py-6 px-3 rounded">
+            <AccordionItemButton className="text-2xl font-bold text-blue-400 flex items-center">
+              <AiFillQuestionCircle className="mr-2"></AiFillQuestionCircle>
+              WHY SHOULD WE USE NODE.JS? AND WHY SHOULD WE USE MONGODB?
             </AccordionItemButton>
           </AccordionItemHeading>
-          <AccordionItemPanel>
-            <p>
-              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-              nostrud velit in irure cillum tempor laboris sed adipisicing eu
-              esse duis nulla non.
+          <AccordionItemPanel className="border-b">
+            <p className="px-4 pt-8">
+              <strong className="underline primary-color font-bold text-justify">
+                Purpose Of NodeJS:
+              </strong>
+              Node.js is an open source, a server-side script which runs on the
+              top of Google’s open-source scripting engine V8. Node.js is fast,
+              lightweight and efficient. It uses the asynchronous mode of
+              operation, event-driven Input/Output rather than using the
+              traditional threads or separate threads for each process.These are
+              the libraries which are built by the awesome community which will
+              solve almost all the generic problems related to the
+              Node.js.Overall it increases the efficiency of the development
+              process as it fills the gap between frontend and backend
+              applications.
+            </p>
+            <p className="px-4 py-8">
+              <strong className="underline primary-color font-bold">
+                Purpose Of MongoDB:
+              </strong>
             </p>
           </AccordionItemPanel>
         </AccordionItem>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton>
-              Is free will real or just an illusion?
+        <AccordionItem className="box-shadow border-b rounded duration-500">
+          <AccordionItemHeading className="  py-6 px-3 rounded">
+            <AccordionItemButton className="text-2xl font-bold text-blue-400 flex items-center">
+              <AiFillQuestionCircle className="mr-2"></AiFillQuestionCircle>
+              WHAT IS THE DIFFERENCE BETWEEN JAVASCRIPT AND NODE.JS?
             </AccordionItemButton>
           </AccordionItemHeading>
-          <AccordionItemPanel>
-            <p>
-              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-              nostrud velit in irure cillum tempor laboris sed adipisicing eu
-              esse duis nulla non.
+          <AccordionItemPanel className="border-y ">
+            <p className="px-4 pt-8">
+              <strong className="underline primary-color font-bold text-justify">
+                JavaScript:
+              </strong>
+              Javascript is a programming language that is used for writing
+              scripts on the website. It is basically used on the client-side.
+              It is capable enough to add HTML and play with the DOM. Javascript
+              can run in any browser engine as like JS core in safari and
+              Spidermonkey in Firefox. It is the upgraded version of ECMA scrip
+              that uses Chrome’s V8 engine written in C++. Some of the
+              javascript frameworks are RamdaJS, TypedJS, etc.
+            </p>
+            <p className="px-4 py-8">
+              <strong className="underline primary-color font-bold">
+                NodeJS:
+              </strong>
+              NodeJS is a Javascript runtime environment. It is mostly used on
+              the server-side. Nodejs does not have capability to add HTML tags.
+              V8 is the Javascript engine inside of node.js that parses and runs
+              Javascript. Nodejs is written in C, C++ and Javascript. Some of
+              the Nodejs modules are Lodash, express etc. These modules are to
+              be imported from npm.
             </p>
           </AccordionItemPanel>
         </AccordionItem>
-        <AccordionItem>
-          <AccordionItemHeading>
-            <AccordionItemButton>
-              Is free will real or just an illusion?
+        <AccordionItem className="box-shadow border-b rounded duration-500">
+          <AccordionItemHeading className=" py-6 px-3 rounded">
+            <AccordionItemButton className="text-2xl font-bold text-blue-400 flex items-center">
+              <AiFillQuestionCircle className="mr-2"></AiFillQuestionCircle>
+              WHAT IS THE DIFFERENCE BETWEEN JAVASCRIPT AND NODE.JS?
             </AccordionItemButton>
           </AccordionItemHeading>
           <AccordionItemPanel>
-            <p>
-              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
-              nostrud velit in irure cillum tempor laboris sed adipisicing eu
-              esse duis nulla non.
+            <p className="px-4 pt-8">
+              <strong className="underline primary-color font-bold text-justify">
+                JavaScript:
+              </strong>
+              Javascript is a programming language that is used for writing
+              scripts on the website. It is basically used on the client-side.
+              It is capable enough to add HTML and play with the DOM. Javascript
+              can run in any browser engine as like JS core in safari and
+              Spidermonkey in Firefox. It is the upgraded version of ECMA scrip
+              that uses Chrome’s V8 engine written in C++. Some of the
+              javascript frameworks are RamdaJS, TypedJS, etc.
+            </p>
+            <p className="px-4 py-8">
+              <strong className="underline primary-color font-bold">
+                NodeJS:
+              </strong>
+              NodeJS is a Javascript runtime environment. It is mostly used on
+              the server-side. Nodejs does not have capability to add HTML tags.
+              V8 is the Javascript engine inside of node.js that parses and runs
+              Javascript. Nodejs is written in C, C++ and Javascript. Some of
+              the Nodejs modules are Lodash, express etc. These modules are to
+              be imported from npm.
             </p>
           </AccordionItemPanel>
         </AccordionItem>
