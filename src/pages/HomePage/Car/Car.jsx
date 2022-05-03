74e1361ae177ae983ce4550b334b9f3123ba25a0
NishatFariza <nishatfarzia78@gmail.com>
2022-05-03 23:58:44 +0600

    use navigate add in update button

--- src/pages/HomePage/Car/Car.jsx
+++ src/pages/HomePage/Car/Car.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 
 const Car = ({ car }) => {
+  const navigate = useNavigate();
+
   const { name, img, price, description, supplier, quantity } = car;
   return (
     <div className="border rounded-2xl box-shadow duration-500 cursor-pointer">
@@ -29,7 +31,10 @@ const Car = ({ car }) => {
             ? `${description.slice(0, 150)}...`
             : description}
         </p>
-        <button className="border border-blue-400 px-6 py-2 rounded-full hover:bg-blue-400 hover:text-white duration-500 mt-4 font-semibold text-stone-600">
+        <button
+          onClick={() => navigate(`/inventory/:${id}`)}
+          className="border border-blue-400 px-6 py-2 rounded-full hover:bg-blue-400 hover:text-white duration-500 mt-4 font-semibold text-stone-600"
+        >
           Stock Update
         </button>
       </div>
