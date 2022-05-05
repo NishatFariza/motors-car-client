6ff319e890096ba91772703a5f62315ea59a3b53
NishatFariza <nishatfarzia78@gmail.com>
2022-05-06 01:23:46 +0600

    add item form in error handle

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
+import auth from "../../firebase.init";
 
 const AddItem = () => {
+  const [user] = useAuthState(auth);
+
   const handleAddItem = (event) => {
     event.preventDefault();
     const quantity = parseInt(event.target.quantity.value);
@@ -55,9 +59,11 @@ const AddItem = () => {
             <input
               type="email"
               name="email"
+              value={user?.email}
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder=" "
-              required
+              readOnly
+              disabled
             />
             <label
               htmlFor="email"
--- src/pages/ManageInventory/MangeInventory.jsx
+++ src/pages/ManageInventory/MangeInventory.jsx
@@ -1,37 +1,56 @@
 import axios from "axios";
 import React from "react";
-import toast from "react-hot-toast";
-
+import { RiArrowRightLine } from "react-icons/ri";
 import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 import useCars from "../../Hooks/UseCars";
 
 const MangeInventory = () => {
   const [cars, setCars] = useCars();
+  
 
   const handleDeleteItem = (id) => {
     console.log(id);
     const url = `http://localhost:5000/inventory/${id}`;
     // console.log(url);
-    const confirm = window.confirm("Are You Sure!!");
-    if (confirm) {
-      axios
-        .delete(url)
-        .then(function (response) {
-          console.log(response);
-          if (response.data.deletedCount === 1) {
-            toast.success("Deleted Successfully");
-            const newCars = cars.filter((car) => car._id !== id);
-            setCars(newCars);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(url)
+          .then(function (response) {
+            console.log(response);
+            if (response.data.deletedCount === 1) {
+              Swal.fire("Deleted!", "Your file has been deleted.", "success");
+              const newCars = cars.filter((car) => car._id !== id);
+              setCars(newCars);
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    });
   };
   return (
-    <div className="py-10 mx-10"> 
-
+    <div className="pb-10 mx-10 pt-5">
+      <div className="mb-8 flex justify-end">
+        <Link
+          to="/manageInventory"
+          className="flex justify-center items-center border bg-blue-500 px-6 py-3 rounded-full hover:bg-yellow-400 text-white duration-500 mt-4 font-semibold "
+        >
+          Add Item
+          <RiArrowRightLine className="text-xl ml-2"></RiArrowRightLine>
+        </Link>
+      </div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -75,7 +94,7 @@ const MangeInventory = () => {
                   </th>
                   <td className="px-6 py-4 font-bold ">{name}</td>
                   <td className="px-6 py-4 text-stone-500">{supplier}</td>
-                  <td className="px-6 py-4 font-semibold ">{price}</td>
+                  <td className="px-6 py-4 font-semibold ">${price}</td>
                   <td className="px-6 py-4 text-right">
                     <button
                       onClick={() => handleDeleteItem(_id)}
