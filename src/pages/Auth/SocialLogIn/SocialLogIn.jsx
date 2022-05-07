4a5f17b170a2fa0b94817ba30eff68dbdd6ee030
NishatFariza <nishatfarzia78@gmail.com>
2022-05-07 12:08:49 +0600

    social login handle

--- src/pages/Auth/SocialLogIn/SocialLogIn.jsx
+++ src/pages/Auth/SocialLogIn/SocialLogIn.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import Loading from "../../Loading/Loading";
 
 const SocialLogIn = () => {
+  const navigate = useNavigate();
+  let location = useLocation();
+  let from = location.state?.from?.pathname || "/";
   //get user social logIn
   const [signInWithGoogle, userGoogle, loading, error] =
     useSignInWithGoogle(auth);
@@ -15,8 +18,13 @@ const SocialLogIn = () => {
     toast.error("Something Error");
     console.log(error);
   }
+  useEffect(() => {
+    if (userGoogle) {
+      toast.success("LogIn Successful");
 
- 
+      navigate(from);
+    }
+  }, [userGoogle, from, navigate]);
 
   if (loading) {
     return <Loading></Loading>;
@@ -29,7 +37,7 @@ const SocialLogIn = () => {
         className="bg-slate-200 md:w-8/12 mx-auto md:py-3 py-2 px-5 md:px-6 flex justify-center items-center rounded-full google-btn md:text-lg text-sm "
       >
         <FcGoogle className="mr-4 md:text-3xl text-lg"></FcGoogle>
-        Sign Up With Google
+        LogIn With Google
       </button>
     </div>
   );
